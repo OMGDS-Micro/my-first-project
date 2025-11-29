66601704877d22c7e091358658c47117e3d8e582
Hsin Kang <2ff6dtymxc@privaterelay.appleid.com>
2025-11-29 20:44:51 +0800

    新增 simple_calc.py

---
+++ simple_calc.py
@@ -0,0 +1,11 @@
+# 定義兩個變數
+num1 = 10
+num2 = 5
+
+# 執行加法和乘法
+sum_result = num1 + num2
+product_result = num1 * num2
+
+# 輸出結果
+print(f"數字 {num1} 和 {num2} 的和是: {sum_result}")
+print(f"數字 {num1} 和 {num2} 的積是: {product_result}")
